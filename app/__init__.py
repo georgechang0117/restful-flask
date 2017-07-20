69622d770081a39bcc8906724630722b29f0d52d
George Mac <georgechang0117gmail.com>
2017-07-20 23:55:56 +0800

    Add routes. Add config.py example.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from .views.api.v1.users import api_v1_users
+app.register_blueprint(api_v1_users, url_prefix='/api/v1/users')
 
 from .views.users import users
 app.register_blueprint(users, url_prefix='/users')
