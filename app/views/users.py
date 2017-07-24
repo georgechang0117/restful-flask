76b0eacbce979151c8b5ea596744293f0be1fa82
George Mac <georgechang0117gmail.com>
2017-07-24 21:24:28 +0800

    Add HTTP Basic Auth to users index

--- app/views/users.py
+++ app/views/users.py
@@ -1,16 +1,37 @@
 from ..models.user import User
 import flask
-from flask import Blueprint, request, jsonify, render_template, abort, redirect, url_for
+from flask import Blueprint, request, Response, jsonify, render_template, abort, redirect, url_for
+from functools import wraps
 from jinja2 import TemplateNotFound
 from .. import db
 
 users = Blueprint('users', __name__, template_folder='templates')
+
+def check_auth(username, password):
+  return username == 'robin' and password == 'ischarming'
+  
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 @users.errorhandler(404)
 def page_not_found():
   return abort(404)
 
 @users.route('/', methods=['GET'])
+@requires_auth
 def index():
   users = User.query.all()
   return render_template('users/index.html', users = users)
